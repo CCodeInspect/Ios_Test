5745694990d3a3541ceec799b4cda3420ad05ff3
CCodeInspect <302869907@qq.com>
2024-07-08 12:10:29 +0800

    提交测试

--- base/basePage.py
+++ base/basePage.py
@@ -5,11 +5,22 @@ from appium.webdriver.webdriver import WebDriver
 
 
 class BasePage:
+    black_list = []
+
     def __init__(self, driver: WebDriver = None):
         self.driver = driver
 
     def find(self, by, locator):
-        return self.driver.find_element(by, locator)
+        try:
+            return self.driver.find_element(by, locator)
+        except Exception as e:
+            print("未找到元素，处理异常")
+            for black in self.black_list:
+                # 查找黑名单中的每一个元素
+                eles = self.driver.find_elements(*black)
+                if len(eles) > 0:
+                    return self.find_and_send(by, locator)
+            raise e
 
     def find_and_click(self, by, locator):
         self.find(by, locator).click()
